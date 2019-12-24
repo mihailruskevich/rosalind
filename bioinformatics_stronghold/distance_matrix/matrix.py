192cb62b36b5f05ddd12c26b3b81b07aa36ea12a
Mihail Ruskevich <mihailruskevich@inbox.lv>
2019-12-24 02:32:14 +0200

    distance_matrix

---
+++ bioinformatics_stronghold/distance_matrix/matrix.py
@@ -0,0 +1,17 @@
+from Bio import SeqIO
+
+
+def d(s, t):
+    return len([1 for x, y in zip(s, t) if x != y])
+
+
+def create_matrix(seq_list):
+    n = len(seq_list[0])
+    return [[d(s1, s2) / n for s2 in seq_list] for s1 in seq_list]
+
+
+dna_list = [str(s.seq) for s in SeqIO.parse(open('dna.fas'), 'fasta')]
+d_matrix = create_matrix(dna_list)
+
+for line in d_matrix:
+    print(*line)
