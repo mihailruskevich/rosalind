3d982f3cd5025afe78c6c6a92667af42d750549a
Mihail Ruskevich <mihailruskevich@inbox.lv>
2026-03-11 00:33:41 +0200

    disease carriers - refactoring

--- bioinformatics_stronghold/disease_carriers/disease_carriers.py
+++ bioinformatics_stronghold/disease_carriers/disease_carriers.py
@@ -1,12 +1,17 @@
 import numpy as np
+from numpy.typing import NDArray
 
 
-def probability_of_one_or_two_allele(p_aa):
+def probability_of_one_or_two_allele(p_aa: NDArray[np.float64]) -> NDArray[np.float64]:
     p_a = p_aa ** 0.5
     return p_aa + 2 * p_a * (1 - p_a)
 
 
-with open('data.txt') as [line]:
-    p_aa_list = np.fromstring(line, dtype=float, sep=' ')
-    p_array = probability_of_one_or_two_allele(p_aa_list)
+def calculate_probability_of_one_or_two_allele():
+    p_aa = np.loadtxt('data.txt', dtype=np.float64)
+    p_array = probability_of_one_or_two_allele(p_aa)
     np.savetxt('res.txt', p_array, fmt='%.3f', newline=' ')
+
+
+if __name__ == '__main__':
+    calculate_probability_of_one_or_two_allele()
